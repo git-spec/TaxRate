36df3a342999e07e06072600a7c08464b81bc435
git-spec <58425036+git-spec@users.noreply.github.com>
2021-08-09 21:24:11 +0200

    set data.js

---
+++ data.js
@@ -0,0 +1,72 @@
+const data = [
+    {
+        "Lager": "DE",
+        "Kunde": "DE",
+        "Netto": 135,
+        "Steuersatz": null,
+        "Brutto": null,
+        "Markierung": null
+    },
+    {
+        "Lager": "DE",
+        "Kunde": "CH",
+        "Netto": 135,
+        "Steuersatz": null,
+        "Brutto": null,
+        "Markierung": null
+    },
+    {
+        "Lager": "DE",
+        "Kunde": "FR",
+        "Netto": 135,
+        "Steuersatz": null,
+        "Brutto": null,
+        "Markierung": null
+    },
+    {
+        "Lager": "DE",
+        "Kunde": "US",
+        "Netto": 135,
+        "Steuersatz": null,
+        "Brutto": null,
+        "Markierung": null
+    },
+    {
+        "Lager": "CH",
+        "Kunde": "CH",
+        "Netto": 135,
+        "Steuersatz": null,
+        "Brutto": null,
+        "Markierung": null
+    },
+    {
+        "Lager": "CH",
+        "Kunde": "DE",
+        "Netto": 135,
+        "Steuersatz": null,
+        "Brutto": null,
+        "Markierung": null
+    },
+    {
+        "Lager": "CH",
+        "Kunde": "FR",
+        "Netto": 135,
+        "Steuersatz": null,
+        "Brutto": null,
+        "Markierung": null
+    },
+    {
+        "Lager": "CH",
+        "Kunde": "US",
+        "Netto": 135,
+        "Steuersatz": null,
+        "Brutto": null,
+        "Markierung": null
+    }
+];
+
+function setData(arr) {
+    data = arr;
+}
+
+export {data, setData};
